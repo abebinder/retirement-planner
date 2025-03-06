843021c72c5609db79e5b9a8b508cb4b273037d6
Abraham Binder <abebinder@gmail.com>
2025-03-06 02:29:39 +0000

    Add savings/contributions/spend text inputs.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<label for="current-savings">Current Savings:</label>
+<input type="text" id="current-savings" name="current-savings"><br><br>
+<label for="annual-contribution">Annual Contribution (while working):</label>
+<input type="text" id="annual-contribution" name="annual-contribution"><br><br>
+<label for="annual-retirement-spend">Annual Retirement Spend:</label>
+<input type="text" id="annual-retirement-spend" name="annual-retirement-spend"><br><br>
