d6b08a3395f735dbfc9eb27a7471c49e1fd49b26
Abraham Binder <abebinder@gmail.com>
2025-03-09 06:45:53 +0000

    Add inflation + investment rate.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,22 +4,42 @@ let annualContribution : number = $state(0)
 let annualRetirementSpend : number = $state(0)
 
 function calculateYearsToRetirement(currentSavings: number, annualContribution: number, annualRetirementSpend: number): number{
+    let inflationRate:number = 0.03;
+    let investmentRate:number = 0.07;
     //https://www.investopedia.com/terms/f/four-percent-rule.asp
     let retirement_number:number = annualRetirementSpend * 25;
-    let remaining:number = retirement_number - currentSavings;
-    if (remaining <= 0) {
-       return 0;
+    let savings:number = currentSavings;
+    let years:number = 0
+    while (savings < retirement_number){
+        savings = (savings * (1 + investmentRate)) + annualContribution;
+        retirement_number = retirement_number * (1 + inflationRate);
+        years++;
+        console.log(savings,retirement_number, years)
     }
-    return remaining/annualContribution;
+    return years;
 }
 
 </script>
 
+<style>
+    /* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+</style>
+
 <label for="current-savings">Current Savings:</label>
-<input bind:value={currentSavings} type="text" id="current-savings" name="current-savings"><br><br>
+<input bind:value={currentSavings} type="number" id="current-savings" name="current-savings"><br><br>
 <label for="annual-contribution">Annual Contribution (while working):</label>
-<input bind:value={annualContribution} type="text" id="annual-contribution" name="annual-contribution"><br><br>
+<input bind:value={annualContribution} type="number" id="annual-contribution" name="annual-contribution"><br><br>
 <label for="annual-retirement-spend">Annual Retirement Spend:</label>
-<input bind:value={annualRetirementSpend} type="text" id="annual-retirement-spend" name="annual-retirement-spend"><br><br>
+<input bind:value={annualRetirementSpend} type="number" id="annual-retirement-spend" name="annual-retirement-spend"><br><br>
 
 <p> You can retire in {calculateYearsToRetirement(currentSavings, annualContribution, annualRetirementSpend)} years.</p>
