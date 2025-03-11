a935e8095c01cca93417b5a6b7c4dc35e130ad8f
Abraham Binder <abebinder@gmail.com>
2025-03-11 05:08:52 +0000

    Use derived rune for SimulationResult.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	let currentSavings: number = $state(1000000);
+	let initialSavings: number = $state(1000000);
 	let annualContribution: number = $state(150000);
 	let annualRetirementSpend: number = $state(65000);
+	let simulationResult: SimulationResult = $derived.by(calculateYearsToRetirement);
 
 	interface SimulationResult {
 		savingsByYear: number[];
 		yearsUntilRetirement: number;
 	}
 
-	function calculateYearsToRetirement(initialSavings: number, annualContribution: number, annualRetirementSpend: number): SimulationResult {
+	function calculateYearsToRetirement(): SimulationResult {
 		const investmentRate: number = 0.07;
 		//https://www.investopedia.com/terms/f/four-percent-rule.asp
 		const retirement_number: number = annualRetirementSpend * 25;
@@ -37,13 +38,13 @@
 </svelte:head>
 
 <label for="current-savings">Current Savings:</label>
-<input bind:value={currentSavings} type="number" id="current-savings" name="current-savings" /><br /><br />
+<input bind:value={initialSavings} type="number" id="current-savings" name="current-savings" /><br /><br />
 <label for="annual-contribution">Annual Contribution (while working):</label>
 <input bind:value={annualContribution} type="number" id="annual-contribution" name="annual-contribution" /><br /><br />
 <label for="annual-retirement-spend">Annual Retirement Spend:</label>
 <input bind:value={annualRetirementSpend} type="number" id="annual-retirement-spend" name="annual-retirement-spend" /><br /><br />
 
-<p>You can retire in {calculateYearsToRetirement(currentSavings, annualContribution, annualRetirementSpend).yearsUntilRetirement} years.</p>
+<p>You can retire in {simulationResult.yearsUntilRetirement} years.</p>
 
 <style>
 	/* Chrome, Safari, Edge, Opera */
