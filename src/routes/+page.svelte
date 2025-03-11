3e67d25576a91fd6340bcc4cd57e8296c1b86a6b
Abraham Binder <abebinder@gmail.com>
2025-03-11 04:25:37 +0000

    Reformat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,34 @@
 <script lang="ts">
-let currentSavings : number = $state(1000000)
-let annualContribution : number = $state(150000)
-let annualRetirementSpend : number = $state(65000)
+	let currentSavings: number = $state(1000000);
+	let annualContribution: number = $state(150000);
+	let annualRetirementSpend: number = $state(65000);
 
-interface SimulationResult {
-  savingsByYear: number[];
-  yearsUntilRetirement: number;
-}
- 
+	interface SimulationResult {
+		savingsByYear: number[];
+		yearsUntilRetirement: number;
+	}
 
-function calculateYearsToRetirement(initialSavings: number, annualContribution: number, annualRetirementSpend: number): SimulationResult{
-    const investmentRate:number = 0.07;
-    //https://www.investopedia.com/terms/f/four-percent-rule.asp
-    const retirement_number:number = annualRetirementSpend * 25;
-    console.log("Retirement number is ", retirement_number)
-    let savingsByYear:number[] =[];
-    let savings = initialSavings
-    let yearsUntilRetirement:number = 0
-    for (let i =0; i<49; i++){
-      savingsByYear.push(savings)
-      if (savings < retirement_number) {
-        yearsUntilRetirement++;
-      }
-      savings = savings * (1 + investmentRate) + annualContribution;
-      console.log(yearsUntilRetirement, savings)
-    }
-    return {
-      savingsByYear: savingsByYear,
-      yearsUntilRetirement: yearsUntilRetirement,
-    }
-}
-
+	function calculateYearsToRetirement(initialSavings: number, annualContribution: number, annualRetirementSpend: number): SimulationResult {
+		const investmentRate: number = 0.07;
+		//https://www.investopedia.com/terms/f/four-percent-rule.asp
+		const retirement_number: number = annualRetirementSpend * 25;
+		console.log('Retirement number is ', retirement_number);
+		let savingsByYear: number[] = [];
+		let savings = initialSavings;
+		let yearsUntilRetirement: number = 0;
+		for (let i = 0; i < 49; i++) {
+			savingsByYear.push(savings);
+			if (savings < retirement_number) {
+				yearsUntilRetirement++;
+			}
+			savings = savings * (1 + investmentRate) + annualContribution;
+			console.log(yearsUntilRetirement, savings);
+		}
+		return {
+			savingsByYear: savingsByYear,
+			yearsUntilRetirement: yearsUntilRetirement
+		};
+	}
 </script>
 
 <svelte:head>
@@ -38,25 +36,25 @@ function calculateYearsToRetirement(initialSavings: number, annualContribution: 
 	<meta name="description" content="Use this calculator to figure out when you can retire." />
 </svelte:head>
 
-<style>
-    /* Chrome, Safari, Edge, Opera */
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-
-/* Firefox */
-input[type=number] {
-  -moz-appearance: textfield;
-}
-</style>
-
 <label for="current-savings">Current Savings:</label>
-<input bind:value={currentSavings} type="number" id="current-savings" name="current-savings"><br><br>
+<input bind:value={currentSavings} type="number" id="current-savings" name="current-savings" /><br /><br />
 <label for="annual-contribution">Annual Contribution (while working):</label>
-<input bind:value={annualContribution} type="number" id="annual-contribution" name="annual-contribution"><br><br>
+<input bind:value={annualContribution} type="number" id="annual-contribution" name="annual-contribution" /><br /><br />
 <label for="annual-retirement-spend">Annual Retirement Spend:</label>
-<input bind:value={annualRetirementSpend} type="number" id="annual-retirement-spend" name="annual-retirement-spend"><br><br>
+<input bind:value={annualRetirementSpend} type="number" id="annual-retirement-spend" name="annual-retirement-spend" /><br /><br />
 
-<p> You can retire in {calculateYearsToRetirement(currentSavings, annualContribution, annualRetirementSpend).yearsUntilRetirement} years.</p>
+<p>You can retire in {calculateYearsToRetirement(currentSavings, annualContribution, annualRetirementSpend).yearsUntilRetirement} years.</p>
+
+<style>
+	/* Chrome, Safari, Edge, Opera */
+	input::-webkit-outer-spin-button,
+	input::-webkit-inner-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+
+	/* Firefox */
+	input[type='number'] {
+		-moz-appearance: textfield;
+	}
+</style>
