0d4e87e21b279c931b2ac840a23dc6321f641750
Abraham Binder <abebinder@gmail.com>
2025-04-21 04:44:43 +0000

    Add function to generate random annual return based on historical S&P500 data.

--- src/lib/calculator.ts
+++ src/lib/calculator.ts
@@ -1,3 +1,28 @@
+import annualReturnData from './sp_annual_returns.json'
+
+import {quantileSeq, type MathArray} from 'mathjs'
+
+const spReturnPercentiles: number[] = generateSpReturnPercentiles();
+
+function getRandomInt(max:number): number {
+    return Math.floor(Math.random() * max);
+  }
+
+export function randomYearlyPercentReturn(): number {
+    const randomPercent = spReturnPercentiles.at(getRandomInt(100)) as number;
+    return randomPercent/100;
+}
+
+export function generateSpReturnPercentiles(): number[] {
+    let spReturnsByYear:number[] = []
+    for (const e of annualReturnData) {
+        //TODO: Make inflation a seperate modifiable variable for the simulation.
+        const inflationPercent = 3;
+        spReturnsByYear.push(e['return_percent'] - inflationPercent);
+    }
+    let spReturnPercentiles: number[] = quantileSeq(spReturnsByYear, 100) as unknown as number[];;
+    return spReturnPercentiles;
+}
 
 export function calculateRetirementNumber(annualRetirementSpend: number) {
     //https://www.investopedia.com/terms/f/four-percent-rule.asp
