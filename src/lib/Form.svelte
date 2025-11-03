a48072e615fc99dd344c40823a8d751cae68c37d
Abe Binder <abebinder@gmail.com>
2025-11-02 18:17:02 -0800

    Use html Form for input.
    Resolves #9.

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -1,45 +1,57 @@
 <script lang="ts">
 	import { type FormValues, defaultFormValues } from './interfaces';
-	let formValues: FormValues = $state(defaultFormValues());
 	let { updateParentComponent } = $props();
-	$effect(() => updateParentComponent(formValues));
+	function handleSubmit(event: SubmitEvent) {
+		event.preventDefault(); // Prevent default browser form submission (page reload)
+		const newFormValues: FormValues = {
+			initialSavings: Number(event.target[0].value),
+			annualContribution: Number(event.target[1].value),
+			annualRetirementSpend: Number(event.target[2].value),
+			currentAge: Number(event.target[3].value),
+			maxRetirementAge: Number(event.target[4].value)
+		};
+		updateParentComponent(newFormValues);
+	}
 </script>
 
-<label for="current-savings">Current Savings:</label>
-<input
-	bind:value={formValues.initialSavings}
-	type="number"
-	id="current-savings"
-	name="current-savings"
-/><br /><br />
-<label for="annual-contribution">Annual Contribution (while working):</label>
-<input
-	bind:value={formValues.annualContribution}
-	type="number"
-	id="annual-contribution"
-	name="annual-contribution"
-/><br /><br />
-<label for="annual-retirement-spend">Annual Retirement Spend:</label>
-<input
-	bind:value={formValues.annualRetirementSpend}
-	type="number"
-	id="annual-retirement-spend"
-	name="annual-retirement-spend"
-/><br /><br />
-<label for="current-age">Current Age:</label>
-<input
-	bind:value={formValues.currentAge}
-	type="number"
-	id="current-age"
-	name="current-age"
-/><br /><br />
-<label for="max-retirement-age">Max Retirement Age:</label>
-<input
-	bind:value={formValues.maxRetirementAge}
-	type="number"
-	id="max-retirement-age"
-	name="max-retirement-age"
-/><br /><br />
+<form onsubmit={handleSubmit}>
+	<label for="current-savings">Current Savings:</label>
+	<input
+		value={defaultFormValues().initialSavings}
+		type="number"
+		id="current-savings"
+		name="current-savings"
+	/><br /><br />
+	<label for="annual-contribution">Annual Contribution (while working):</label>
+	<input
+		value={defaultFormValues().annualContribution}
+		type="number"
+		id="annual-contribution"
+		name="annual-contribution"
+	/><br /><br />
+	<label for="annual-retirement-spend">Annual Retirement Spend:</label>
+	<input
+		value={defaultFormValues().annualRetirementSpend}
+		type="number"
+		id="annual-retirement-spend"
+		name="annual-retirement-spend"
+	/><br /><br />
+	<label for="current-age">Current Age:</label>
+	<input
+		value={defaultFormValues().currentAge}
+		type="number"
+		id="current-age"
+		name="current-age"
+	/><br /><br />
+	<label for="max-retirement-age">Max Retirement Age:</label>
+	<input
+		value={defaultFormValues().maxRetirementAge}
+		type="number"
+		id="max-retirement-age"
+		name="max-retirement-age"
+	/><br /><br />
+	<input type="submit" value="Submit" />
+</form>
 
 <style>
 	/* Chrome, Safari, Edge, Opera */
